41647fcb5269b257c07a9f791ffc4a19594d3e91
PedroASA <m.amaral.hhs@gmail.com>
2022-11-15 20:43:07 -0300

    Adicionando o projeto do segundo exercício

---
+++ Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vertice v1 = new Vertice(1.0, 1.0);
+            Vertice v2 = new Vertice(1.0, 0.0);
+            Console.WriteLine("{0}\t{1}", v1, v2);
+            Console.WriteLine(v1.Equals(v2));
+
+            Console.WriteLine(v1.Distancia(v2));
+
+            v2.Move(1.0,1.0);
+            Console.WriteLine(v1);
+            Console.WriteLine(v1.Equals(v2));
+
+        }
+    }
+}
---
+++ Ex2/Vertice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex2
+{
+    public class Vertice
+    {
+        // leitura pública e escrita privada
+        public double y { get; private set; }
+        public double x { get; private set; }
+        public Vertice(double x, double y)
+        {
+            (this.x, this.y) = (x, y);
+        }
+
+        public double Distancia(Vertice that)
+            => Math.Sqrt(
+                Math.Pow(this.x - that.x, 2)
+                + Math.Pow(this.y - that.y, 2));
+
+
+        public void Move(double x, double y)
+        {
+            (this.x, this.y) = (x, y);
+        }
+
+        public override bool Equals(object obj)
+            => obj is Vertice && this == (Vertice)obj;
+
+        public override string ToString()
+            => $"({x}, {y})";
+
+        public static bool operator ==(Vertice v1, Vertice v2)
+            => (v1.x == v2.x) && (v1.y == v2.y);
+
+        public static bool operator !=(Vertice v1, Vertice v2)
+            => !(v1 == v2);
+    }
+}
