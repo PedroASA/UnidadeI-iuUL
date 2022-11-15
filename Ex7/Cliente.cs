ff5d92ba99a06d14afd1da5a4bdbf2fe605d3e83
PedroASA <m.amaral.hhs@gmail.com>
2022-11-15 20:43:51 -0300

    Adicionando o projeto do sétimo exercício

---
+++ Ex7/Cliente.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Ex7
+{
+
+    internal class Cliente
+    {
+        private string _nome;
+        private ulong _cpf;
+        private DateTime _dataDeNascimento;
+        private uint _dependentes;
+        internal string Nome
+        {
+            set
+            {
+                if (value.Length >= 5)
+                {
+                    this._nome = value;
+                }
+                else
+                {
+                    throw new InvalidClientException(String.Format("Nome do Cilente deve ter ao menos 5 letras. {0} foram informadas", value.Length));
+                }
+            }
+            private get => _nome;
+        }
+        internal ulong Cpf
+        {
+            set
+            {
+                int tmp = value.ToString().Length;
+                if (tmp == 11)
+                {
+                    this._cpf = value;
+                }
+                else
+                {
+                    throw new InvalidClientException(String.Format("Cpf do Cilente deve ter 11 dígitos. {0} dígitos informados", tmp));
+                }
+            }
+            private get => this._cpf;
+        }
+
+        internal DateTime DataDeNascimento
+        {
+            set
+            {
+                double age = (DateTime.Now - value).TotalDays / 365.2425;
+                if (age >= 18)
+                {
+                    this._dataDeNascimento = value;
+                }
+                else
+                {
+                    throw new InvalidClientException(String.Format("Idade do Cilente deve ser ao menos 18 anos. Idade dada: {0}", age));
+                }
+            }
+            private get => this._dataDeNascimento;
+        }
+        internal float RendaMensal { private get; set; }
+        internal TipoEstadoCivil EstadoCivil { private get; set; }
+        internal uint Dependentes
+        {
+            private get => this._dependentes;
+            set
+            {
+                if (value <= 10)
+                {
+                    this._dependentes = value;
+                }
+                else
+                {
+                    throw new InvalidClientException(String.Format("Número de dependentes do Cilente deve ser no máximo 10. Número informado: {0}", value));
+                }
+            }
+        }
+
+        internal enum TipoEstadoCivil
+        {
+            CASADO,
+            SOLTEIRO,
+            VIUVO,
+            DIVORCIADO
+        }
+
+        public override string ToString()
+        {
+            string[] x = {"Nome do Cliente:", "CPF:", "Data de Nascimento:", "Renda Mensal:", "Estado Civil:", "Dependentes:"};
+            object[] y = {Nome, Cpf, DataDeNascimento.ToString("d", DateTimeFormatInfo.InvariantInfo), RendaMensal.ToString("0.00"), EstadoCivil, Dependentes};
+
+            return x
+                .Zip(y, (first, second) => $"{first, -10}{second, 20}\n")
+                .Aggregate("\nUsuário\n", (acc, x) => acc + x);
+        }
+
+        internal class InvalidClientException : Exception
+        {
+            public InvalidClientException(string message)
+            : base(message)
+            { }
+        }
+    }
+}
---
+++ Ex7/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Cliente c = new();
+            string line;
+
+            string[] messages = { 
+                "Nome do Cliente:", 
+                "CPF:", 
+                "Data de Nascimento:", 
+                "Renda Mensal:", 
+                "Estado Civil:", 
+                "Dependentes:" 
+            };
+
+            Action<string>[] actions =
+            {
+                // Set Nome
+                line => {c.Nome = line.Trim();},
+
+                // Set CPF
+                line => { c.Cpf = ulong.Parse(line); },
+
+                // Set Data de Nascimento
+                line =>
+                {
+                    var data = line.Split('/').Select(Int32.Parse).Reverse();
+                    c.DataDeNascimento = new DateTime(data.First(), data.ElementAt(1), data.Last());
+                },
+
+                // Set Renda Mensal
+                line => 
+                {
+                    //if(line.Split(",").Count() > 0)
+                    //{
+
+                    //}
+                    c.RendaMensal = float.Parse(line);
+                },
+
+                // Set Estado Civil
+                line =>
+                {
+                    if(line.Length != 1)
+                    {
+                        throw new InvalidEstadoCivilException("Estado Civil deve conter apenas um caracter!");
+                    } else
+                    {
+                        switch(Char.ToUpper(line[0]))
+                        {
+                            case 'C':
+                                c.EstadoCivil = Cliente.TipoEstadoCivil.CASADO;
+                                break;
+                            case 'S':
+                                c.EstadoCivil = Cliente.TipoEstadoCivil.SOLTEIRO;
+                                break;
+                            case 'V':
+                                c.EstadoCivil = Cliente.TipoEstadoCivil.VIUVO;
+                                break;
+                            case 'D':
+                                c.EstadoCivil = Cliente.TipoEstadoCivil.DIVORCIADO;
+                                break;
+                            default:
+                                throw new InvalidEstadoCivilException("Entrada Inválida. Estado Civil deve ser {C | S | V | D}");
+                        }
+                    }
+                },
+
+                // Set Dependentes
+                line => {c.Dependentes = uint.Parse(line);}
+            };
+
+
+            foreach (var (message, action) in messages.Zip(actions))
+            {
+                do
+                {
+                    Console.WriteLine(message);
+                    line = Console.ReadLine();
+                    try
+                    {
+                        action(line);
+                        break;
+                    }
+                    catch (Cliente.InvalidClientException e)
+                    {
+                        Console.WriteLine("Erro: {0}", e.Message);
+                    }
+                    catch (Exception e) when (e is OverflowException || e is FormatException)
+                    {
+                        Console.WriteLine("Erro: Valor informado não é sequencia de dígitos\n{0}", e.Message);
+                    }
+                    catch (ArgumentOutOfRangeException e) 
+                    {
+                        Console.WriteLine("Erro: Data Informada não é válida\n{0}", e.Message);
+                    } 
+                    catch (InvalidEstadoCivilException e)
+                    {
+                        Console.WriteLine("Erro: Estado Civil informado é inválido.\n{0}", e.Message);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Erro: Algo inesperado ocorreu.\n{0}", e.Message);
+                    }
+                    continue;
+                } while (true);
+            }
+
+            Console.WriteLine(c);
+        }
+
+        private class InvalidEstadoCivilException : Exception
+        {
+            public InvalidEstadoCivilException(string message)
+            : base(message)
+            { }
+        }
+    }
+}
