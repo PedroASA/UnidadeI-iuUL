04f044e90eaf6f30b22f3dd99626843ecc5788f9
PedroASA <m.amaral.hhs@gmail.com>
2022-11-15 20:43:29 -0300

    Adicionando o projeto do quinto exercício

---
+++ Ex5/Intervalo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex5
+{
+    public class Intervalo
+    {
+        public readonly DateTime inicial, final;
+
+        public readonly TimeSpan Duracao;
+
+    public Intervalo(DateTime inicial, DateTime final) 
+        {
+            if (inicial > final)
+            {
+                throw new Exception(String.Format("Exception: Data inicial deve ser anterior à data final do intervalo\nErro: {0} > {1}", inicial, final));
+            }
+            this.inicial = inicial;
+            this.final = final;
+
+            this.Duracao = this.final - this.inicial;
+        }
+        /*
+         * Um intervalo X tem interseção com um intervalo Y
+         * se X.final ou X.incio pertencem a [Y.incio, Y.final],
+         * ou vice-versa
+         */
+        public bool TemIntersecao(Intervalo i)
+        {
+            return (this.inicial <= i.inicial && i.inicial <= this.final)
+                || (this.inicial <= i.final && i.final <= this.final);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            var that = (Intervalo) obj;
+            return this.inicial == that.inicial && this.final == that.final;
+        }
+        // Imprimir o intervalo no exercício 6
+        public override string ToString()
+            => $"{inicial} => {final}";
+    }
+}
---
+++ Ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Intervalo i = new(new DateTime(2010, 1, 1, 8, 0, 15), new DateTime(2010, 1, 1, 8, 0, 10));
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            Intervalo _i = new(new DateTime(2010, 1, 1, 8, 0, 15), new DateTime(2010, 8, 18, 13, 30, 30));
+            Intervalo _i2 = new(new DateTime(2010, 1, 1, 8, 0, 1), new DateTime(2010, 1, 1, 8, 0, 15));
+
+            Console.WriteLine(_i.TemIntersecao(_i2));
+            Console.WriteLine(_i.Equals(_i2));
+            Console.WriteLine(_i2.Duracao);
+        }
+    }
+}
