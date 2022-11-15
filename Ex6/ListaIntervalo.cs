f4de0c4524159ad68b8d52f0f0e5f43decaf93d7
PedroASA <m.amaral.hhs@gmail.com>
2022-11-15 20:43:37 -0300

    Adicionando o projeto do sexto exercício

---
+++ Ex6/ListaIntervalo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex5;
+
+namespace Ex6
+{
+    internal class ListaIntervalo
+    {
+        private readonly List<Intervalo> intervalos;
+        internal ListaIntervalo() 
+        { 
+            intervalos = new List<Intervalo>();
+        }
+
+        internal bool Add(Intervalo i)
+        {
+            // Verifica interseções
+            foreach (var e in intervalos)
+            {
+                if (e.TemIntersecao(i))
+                {
+                    return false;
+                }
+            }
+            // Encontra posição na lista ordenada
+            var index = find(i);
+
+            intervalos.Insert(index, i);
+            return true;
+        }
+
+        internal void Imprime()
+            => intervalos.ForEach(Console.WriteLine);
+        
+        /*
+         * A busca é feita de forma sequencial, pois já se
+         * consome O(N) para verificar que não há interseções com 
+         * os elementos da lista. Assim, não é necessário fazer uma busca em O(lg(N)),
+         * visto que O(lg(N)) + O(N) = O(N) + O(N) = O(N).
+         */
+        private int find(Intervalo i)
+        {
+            var tmp = i.inicial;
+            int index = -1;
+            for (int j = 0; j < intervalos.Count; j++)
+            {
+                /*
+                 * Basta ordenar por inícial, visto que dois intervalos 
+                 * não podem ter o mesmo inicial (não tem interseção).
+                 */
+                if (intervalos[j].inicial > tmp)
+                {
+                    index = j;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                index = intervalos.Count;
+            }
+            return index;
+        }
+   
+    }
+}
---
+++ Ex6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Ex5;
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaIntervalo li = new ListaIntervalo();
+
+            bool tmp;
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2010, 1, 1, 8, 0, 15), 
+                new DateTime(2010, 1, 1, 8, 0, 19)));
+
+            Console.WriteLine(tmp);
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2010, 1, 1, 8, 0, 15),
+                new DateTime(2010, 1, 1, 8, 0, 18)));
+
+            Console.WriteLine(tmp);
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2010, 1, 1, 8, 0, 30),
+                new DateTime(2010, 1, 1, 8, 0, 58)));
+
+            Console.WriteLine(tmp);
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2005, 1, 1, 8, 0, 30),
+                new DateTime(2005, 1, 1, 8, 0, 58)));
+
+            Console.WriteLine(tmp);
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2007, 1, 1, 8, 0, 30),
+                new DateTime(2007, 1, 1, 8, 0, 58)));
+
+            Console.WriteLine(tmp);
+
+            tmp = li.Add(new Intervalo(
+                new DateTime(2000, 1, 1, 8, 0, 30),
+                new DateTime(2000, 1, 1, 8, 0, 58)));
+
+            Console.WriteLine(tmp);
+
+            li.Imprime();
+
+        }
+    }
+}
