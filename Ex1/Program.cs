053ba626ee9f1ea5c0c488e6c7422fe00793eabb
PedroASA <m.amaral.hhs@gmail.com>
2022-11-16 11:03:20 -0300

    Projeto Ex1 comentado  :page_with_curl: e revisado :heavy_check_mark:

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 
+/*
+ * Classe Program e classe Piramide estão no mesmo namespace (Ex1),
+ * então o modificador "internal" é sufuciente para que elas se "vejam"
+ */
 namespace Ex1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            int intTemp = Convert.ToInt32(args[0]);
-            Piramide p = new Piramide(intTemp);
-            p.Desenha();
+            try
+            {
+                int intTemp = Convert.ToInt32(args[0]);
+                Piramide p = new Piramide(intTemp);
+                p.Desenha();
+            }catch(Piramide.NonPositiveIntegerException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Argumento de linha de comando deve ser um inteiro de até 32 bits.\n{0}", e.Message);
+            }
         }
     }
 }
