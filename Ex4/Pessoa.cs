5e769938360b89535b508f84b94531dbc0626494
PedroASA <m.amaral.hhs@gmail.com>
2022-11-20 17:27:44 -0300

    Adicionando projeto Ex4

---
+++ Ex4/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ex4
+{
+    internal class Pessoa
+    {
+
+        private readonly string nome;
+
+        private CertidaoNascimento Certidao { get; set; }
+
+        internal Pessoa(string nome) 
+        {
+            this.nome = nome;
+        }
+
+        internal void SetCertidao(DateTime d)
+        {
+            Certidao = new(this, d);
+        }
+
+        public override string ToString()
+        {
+            return $"{nome}\t{(Certidao == null ? "N/A" : Certidao)}";
+        }
+
+        // Certidão de Nascimento só pode ser criada
+        // pela classe Pessoa. Assim, uma 
+        private class CertidaoNascimento
+        {
+            private readonly DateTime dataDeEmissao;
+
+            private readonly Pessoa p;
+
+            internal CertidaoNascimento(Pessoa p, DateTime dataDeEmissao)
+            {
+                this.dataDeEmissao = dataDeEmissao;
+                this.p = p;
+            }
+
+            public override string ToString()
+            {
+                return $"{dataDeEmissao}";
+            }
+        }
+    }
+}
---
+++ Ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new("Pedro");
+
+            p.SetCertidao(new DateTime(2000, 1, 1));
+
+            Console.WriteLine(p);
+        }
+    }
+}
