d8a7fb0980238e531761aaba0615eae633221ae5
PedroASA <m.amaral.hhs@gmail.com>
2022-11-20 19:34:39 -0300

    Adicionando projeto Ex7

---
+++ Ex7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Propriedades p = new (args[0]);
+
+            p.AddKey("Vasco", "Da Gama");
+
+            p.GetValue("Vasco", out string _val);
+
+            Console.WriteLine(_val);
+
+            p.UpdateValue("Vasco", "Gigante");
+
+            Console.WriteLine(p.Contains("Vasco"));
+
+            try
+            {
+                p.AddKey("Vasco", "Exceção");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                p.UpdateValue("Chave Não Existe", "Exceção");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            p.WriteToFile(args[1]).Wait();
+        }
+    }
+}
---
+++ Ex7/Propriedades.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex7
+{
+    internal class Propriedades
+    {
+        private readonly Dictionary<string, string> propriedades = new();
+
+        internal Propriedades() { }
+
+        internal Propriedades(string path)
+        {
+            ReadFile(path);
+        }
+
+        private void ReadFile(string path)
+        {
+            Parallel.ForEach(System.IO.File.ReadLines(path),
+                line =>
+                {
+                    var tmp = line.Split('=');
+                    if (tmp.Length == 2)
+                    {
+                        AddKey(tmp[0], tmp[1]);
+                    }
+                }); 
+        }
+
+        internal bool GetValue(string key, out string _val)
+        {
+            return propriedades.TryGetValue(key, out _val);
+        }
+
+        internal void UpdateValue(string key, string val)
+        {
+            if(propriedades.ContainsKey(key))
+                propriedades[key] = val;
+            else
+                throw new Exception("Chave não existe");
+        }
+
+        internal bool Contains(string key)
+        {
+            return propriedades.ContainsKey(key);
+        }
+
+        internal void AddKey(string key, string value)
+        {
+            if(Contains(key))
+            {
+                throw new Exception("Chave já existe");
+            }
+            propriedades.Add(key, value);
+        }
+
+        internal async Task WriteToFile(string path)
+        {
+            var lines =
+                propriedades
+                .Keys
+                .Zip(propriedades.Values, (k, v) => k + "=" + v);
+
+            await File.WriteAllLinesAsync(path, lines);
+        }
+    }
+}
