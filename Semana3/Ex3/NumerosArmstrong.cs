1f87b42f1031cde767f3d254a217bea8e9f4f257
PedroASA <m.amaral.hhs@gmail.com>
2022-12-06 10:46:00 -0300

    Projeto 3 Comentado e Revisado

--- Semana3/Ex3/NumerosArmstrong.cs
+++ Semana3/Ex3/NumerosArmstrong.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex3
 {
     internal static class NumerosArmstrong
     {
+        // Método de Extensão da Classe Uint
         public static bool IsArmstrong(this uint valor)
         {
+            // Número de dígitos do número $valor
             int x = (int)Math.Log10((double)valor) + 1;
+
             uint temp, num = valor, sum=0, r;
             for (temp = num; num != 0; num = num / 10)
             {
