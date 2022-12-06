584daa085bfd1713d41524f7782b49682dd93cfd
PedroASA <m.amaral.hhs@gmail.com>
2022-12-06 10:46:15 -0300

    Projeto 4 Comentado e Revisado

--- Semana3/Ex4/ListExtension.cs
+++ Semana3/Ex4/ListExtension.cs
@@ -5,10 +5,13 @@ namespace Ex4
 {
     internal static class ListExtension
     {
+        // Método de Extensão da Classe List<T>
         public static void RemoveRepetidos<T>(this List<T> list)
         {
-            HashSet<T> set = new HashSet<T>();
+            // Elementos percorridos
+            HashSet<T> set = new ();
 
+            // Percorre a lista de trás para frente para já remover elemento repetido ao passar
             for (int i = list.Count-1; i >= 0; --i)
             {
                 if (set.Contains(list[i]))
